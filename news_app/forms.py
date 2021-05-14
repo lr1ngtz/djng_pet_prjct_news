af94b61bf0db859bac22db39d02349c3324fb5a4
lr1n <sko0hlol@gmail.com>
2021-05-15 01:33:26 +0200

    Add handling form that is not related with our models

--- news_app/forms.py
+++ news_app/forms.py
@@ -4,7 +4,17 @@ from .models import Category, News
 
 
 class NewsForm(forms.Form):
-    title = forms.CharField(max_length=150)
-    content = forms.CharField()
-    is_published = forms.BooleanField()
-    category = forms.ModelChoiceField(queryset=Category.objects.all())
+    title = forms.CharField(
+        max_length=150,
+        widget=forms.TextInput(attrs={"class": "form-control"})
+    )
+    content = forms.CharField(
+        widget=forms.Textarea(attrs={"class": "form-control", "rows": 5})
+    )
+    # "label" works like "verbose_name" in models
+    is_published = forms.BooleanField(label="Is published?:", initial=True)
+    category = forms.ModelChoiceField(
+        queryset=Category.objects.all(),
+        empty_label="Choose category",
+        widget=forms.Select(attrs={"class": "form-control"})
+    )
