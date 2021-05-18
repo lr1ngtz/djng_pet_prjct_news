a4e94b6e3e7cb72b28ca82084269f88937c9d765
lr1n <sko0hlol@gmail.com>
2021-05-18 11:12:54 +0200

    Add controller(view) based on 'CreateView'

--- news_app/views.py
+++ news_app/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from django.views.generic import ListView, DetailView
+from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DetailView, ListView
 
-from .models import Category, News
 from .forms import NewsForm
+from .models import Category, News
 
 
 class HomeNews(ListView):
@@ -45,6 +46,14 @@ class ViewNews(DetailView):
     context_object_name = "news_item"
 
 
+class CreateNews(CreateView):
+    form_class = NewsForm
+    template_name = "news_app/add_news.html"
+    # We define 'success_url' if we didn't define
+    # 'get_absolute_url' in our Model
+    # success_url = reverse_lazy("home")
+
+
 # def index(request):
 #     news = News.objects.all()
 #     context = {
@@ -64,23 +73,23 @@ def get_category(request, category_id):
     return render(request, "news_app/category.html", context=context)
 
 
-def view_news(request, news_id):
-    # news_item = News.objects.get(pk=news_id)
-    news_item = get_object_or_404(News, pk=news_id)
-    context = {"news_item": news_item}
-    return render(request, 'news_app/view_news.html', context=context)
+# def view_news(request, news_id):
+#     # news_item = News.objects.get(pk=news_id)
+#     news_item = get_object_or_404(News, pk=news_id)
+#     context = {"news_item": news_item}
+#     return render(request, 'news_app/view_news.html', context=context)
 
 
-def add_news(request):
-    if request.method == "POST":
-        form = NewsForm(request.POST)
-        if form.is_valid():
-            # # It's for non-related forms "forms.Form"
-            # news = News.objects.create(**form.cleaned_data)
-            news = form.save()
-            return redirect(news)
-    else:
-        form = NewsForm()
+# def add_news(request):
+#     if request.method == "POST":
+#         form = NewsForm(request.POST)
+#         if form.is_valid():
+#             # # It's for non-related forms "forms.Form"
+#             # news = News.objects.create(**form.cleaned_data)
+#             news = form.save()
+#             return redirect(news)
+#     else:
+#         form = NewsForm()
 
-    context = {"form": form}
-    return render(request, "news_app/add_news.html", context)
+#     context = {"form": form}
+#     return render(request, "news_app/add_news.html", context)
