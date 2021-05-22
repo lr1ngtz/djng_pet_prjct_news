8b4846ae9de1e77a04cb00ec0ffbb5ca914ae505
lr1n <sko0hlol@gmail.com>
2021-05-22 12:18:01 +0200

    Add filtering categories by news(exist or not exist)

--- news_app/templatetags/news_tags.py
+++ news_app/templatetags/news_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.db.models import Count
 
 from ..models import Category
 
@@ -12,5 +13,6 @@ def get_categories():
 
 @register.inclusion_tag("news_app/categories_list.html")
 def show_categories():
-    categories = Category.objects.all()
+    # categories = Category.objects.all()
+    categories = Category.objects.annotate(cnt=Count('news')).filter(cnt__gt=0)
     return {"categories": categories}
