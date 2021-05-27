7b03c7362ed80bda70e773f515835db8599c5893
lr1n <sko0hlol@gmail.com>
2021-05-27 12:13:21 +0200

    Customize our admin page

--- news_app/admin.py
+++ news_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
-from .models import News, Category
+from .models import Category, News
 
 
 class NewsAdmin(admin.ModelAdmin):
@@ -11,12 +12,35 @@ class NewsAdmin(admin.ModelAdmin):
         'created_at',
         'updated_at',
         'is_published',
-        'views'
+        'views',
+        'get_photo'
     )
     list_display_links = ('id', 'title')
     search_fields = ('title', 'content')
     list_editable = ('is_published',)
     list_filter = ('is_published', 'category')
+    fields = (
+        'title',
+        'category',
+        'content',
+        'photo',
+        'get_photo',
+        'is_published',
+        'views',
+        'created_at',
+        'updated_at',
+    )
+    readonly_fields = ('get_photo', 'views', 'created_at', 'updated_at')
+    save_on_top = True
+
+    def get_photo(self, obj):
+        if obj.photo:
+            return mark_safe(f'<img src="{obj.photo.url}", width="270">')
+        else:
+            return '-'
+
+    # We set here a name for 'list_display'
+    get_photo.short_description = 'Image'
 
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -27,3 +51,6 @@ class CategoryAdmin(admin.ModelAdmin):
 
 admin.site.register(News, NewsAdmin)
 admin.site.register(Category, CategoryAdmin)
+
+admin.site.site_title = 'News Management'
+admin.site.site_header = 'News Management'
