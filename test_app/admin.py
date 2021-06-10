d055d6362333368b79f018d245a49cd7a2df5d96
lr1n <sko0hlol@gmail.com>
2021-06-10 23:44:14 +0200

    Add some customization for our multilevel-tree of objects (Rubrics)

--- test_app/admin.py
+++ test_app/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
+# from mptt.admin import MPTTModelAdmin
+from mptt.admin import DraggableMPTTAdmin
 
-from test_app.models import Rubric
+from test_app.models import Rubric, Article
 
 
-admin.site.register(Rubric)
+# admin.site.register(Rubric, MPTTModelAdmin)
+admin.site.register(
+    Rubric,
+    DraggableMPTTAdmin,
+    list_display=(
+        'tree_actions',
+        'indented_title',
+        # ...more fields if you feel like it...
+    ),
+    list_display_links=(
+        'indented_title',
+    ),
+)
+admin.site.register(Article)
